29fecd9ecd050522dbe1daa20ce2a7c8f47d198b
GarryMcKee <gmk0232@gmail.com>
2024-12-03 11:46:51 +0000

    Remove unused import

--- app/src/main/java/com/gmk0232/whosthatpokemon/MainActivity.kt
+++ app/src/main/java/com/gmk0232/whosthatpokemon/MainActivity.kt
@@ -3,7 +3,6 @@ package com.gmk0232.whosthatpokemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
