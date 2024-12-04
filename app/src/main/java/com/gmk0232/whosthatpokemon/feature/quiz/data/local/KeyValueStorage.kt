b1aed459c3763e2b612e63f7b354e747ced444ca
GarryMcKee <gmk0232@gmail.com>
2024-12-04 23:14:09 +0000

    Remove more redundant imports

--- app/src/main/java/com/gmk0232/whosthatpokemon/feature/quiz/data/local/KeyValueStorage.kt
+++ app/src/main/java/com/gmk0232/whosthatpokemon/feature/quiz/data/local/KeyValueStorage.kt
@@ -2,14 +2,12 @@ package com.gmk0232.whosthatpokemon.feature.quiz.data.local
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.single
 
 class KeyValueStorage(private val context: Context) {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
