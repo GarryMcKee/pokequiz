824c03b9ca46e8f72201f77065adc4675119ba8f
GarryMcKee <gmk0232@gmail.com>
2024-12-04 23:13:12 +0000

    Remove redundant classes

--- app/src/main/java/com/gmk0232/whosthatpokemon/feature/quiz/domain/SetScoreUseCase.kt
+++
@@ -1,5 +0,0 @@
-package com.gmk0232.whosthatpokemon.feature.quiz.domain
-
-interface SetScoreUseCase {
-    suspend fun execute(score: Int)
-}
--- app/src/main/java/com/gmk0232/whosthatpokemon/feature/quiz/domain/SetScoreUseCaseImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.gmk0232.whosthatpokemon.feature.quiz.domain
-
-import com.gmk0232.whosthatpokemon.feature.quiz.data.local.KeyValueStorage
-
-class SetScoreUseCaseImpl(private val keyValueStorage: KeyValueStorage) : SetScoreUseCase {
-    override suspend fun execute(score: Int) {
-        keyValueStorage.setInt(SCORE_PREFERENCE_KEY, score)
-    }
-}
